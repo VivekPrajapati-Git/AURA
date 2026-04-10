4acbcab610ee611637beb55c88f25704a464c924
Hellnight2005 <abhijeet2005shinde@gmail.com>
2026-04-10 15:17:51 +0530

    feat: Add Morgan HTTP request logger

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
